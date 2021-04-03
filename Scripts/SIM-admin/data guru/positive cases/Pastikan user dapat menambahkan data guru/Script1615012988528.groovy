14f567a1082639fe9c599cb45d1d2148314089b6
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2021-04-03 16:14:45 +0700

    lupa hapus library

--- Scripts/SIM-admin/data guru/positive cases/Pastikan user dapat menambahkan data guru/Script1615012988528.groovy
+++ Scripts/SIM-admin/data guru/positive cases/Pastikan user dapat menambahkan data guru/Script1615012988528.groovy
@@ -12,9 +12,6 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
 
 'User click Data Guru on side menu\r'
 WebUI.click(findTestObject('admin/Page_Beranda - PIJAR/span_Data Guru'))
